795862f2e7cf393aa6656b9b4ecb95dc65d7cdb2
Cécile Andrisi <cecile.andrisi@gmail.com>
2025-06-23 15:21:16 +0200

    Modifié style bouton modale

--- template-parts/modal-contact.php
+++ template-parts/modal-contact.php
@@ -1,7 +1,9 @@
 <div class="modal" id="modal">
     <div class="modal-content">
         <button class="close-modal" aria-label="Fermer la modale">&times;</button>
-        <h2>Contactez-nous</h2>
+        <div class="contact-header">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-header/contact-header.png" alt="contact header" />
+                        </div>
         <?php echo do_shortcode('[contact-form-7 id="3c6e00f" title="Contact"]'); ?>
     </div>
 </div>
